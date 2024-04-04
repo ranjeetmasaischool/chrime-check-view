97666b8a12dbc5db2efd87bd72e768c3f0576e28
Ranjeet Kumar <ranjeet030200@gmail.com>
2024-04-04 12:27:14 +0530

    add sense

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 import Footer from "@/components/Footer";
 export const metadata = {
+   name:"google-adsense-account", content:"ca-pub-2333627269955484",
   title: "Arrest India",
   description: "Search crime data of indian pepole using there name, Aadhar number, State, Mobile Number and other Details",
   
