a8d764542fc6f76118ba5fa5aaa9344f4c8165ba
Ranjeet Kumar <ranjeet030200@gmail.com>
2024-04-04 12:39:36 +0530

    head add

--- app/layout.js
+++ app/layout.js
@@ -3,17 +3,21 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 import Footer from "@/components/Footer";
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2333627269955484"
-     crossorigin="anonymous"></script>
+
 export const metadata = {
   title: "Arrest India",
   description: "Search crime data of indian pepole using there name, Aadhar number, State, Mobile Number and other Details",
   
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2333627269955484"
+     crossorigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
